99e55a8efe0157af95fd0b4ac884853772f4a09c
Bruno Noveli <brunimzuppwolf@gmail.com>
2024-01-31 16:17:14 -0300

    Feito lógica para acessar as Mensagens sem a necessidade de passar o context para a Controller

--- lib/lab_clinicas_core.dart
+++ lib/lab_clinicas_core.dart
@@ -1,2 +1,7 @@
 library lab_clinicas_core;
 
+export 'src/fp/either.dart';
+export 'src/fp/unit.dart';
+export 'src/fp/nil.dart';
+
+export 'src/helpers/messages.dart';
--- lib/src/helpers/messages.dart
+++ lib/src/helpers/messages.dart
@@ -1,5 +1,6 @@
 
 import 'package:flutter/material.dart';
+import 'package:signals_flutter/signals_flutter.dart';
 import 'package:top_snackbar_flutter/custom_snack_bar.dart';
 import 'package:top_snackbar_flutter/top_snack_bar.dart';
 
@@ -24,5 +25,67 @@ final class Messages {
       Overlay.of(context), 
       CustomSnackBar.success(message: message),
     );
+  }
+}
+
+// Mixin responsável pelo controle das mensagens
+mixin MessageStateMixin {
+
+  final Signal<String?> _errorMessage = signal(null);
+  String? get errorMessage => _errorMessage();
+
+  final Signal<String?> _infoMessage = signal(null);
+  String? get infoMessage => _infoMessage();
+
+  final Signal<String?> _successMessage = signal(null);
+  String? get successMessage => _successMessage();
+
+  void clearError() => _errorMessage.value = null;
+  void clearInfo() => _infoMessage.value = null;
+  void clearSuccess() => _successMessage.value = null;
+
+  void showError(String message) {
+    
+    // Tudo que eu rodar aqui não vai notificar quem está usando as variáveis reativas
+    untracked(() => clearError());
+
+    _errorMessage.value = message;
+  }
+
+  void showInfo(String message) {
+    
+    untracked(() => clearInfo());
+    _infoMessage.value = message;
+  }
+
+  void showSuccess(String message) {
+    
+    untracked(() => clearSuccess());
+    _successMessage.value = message;
+  }
+
+  void clearAllMessages() {
+    untracked(() {
+      clearError();
+      clearInfo();
+      clearSuccess();
+    });
+  }
+}
+
+// Mixin responsável por mostrar as mensagens
+mixin MessageViewMixin<T extends StatefulWidget> on State {
+
+  void messageListener(MessageStateMixin state) {
+    effect(() {
+      switch(state) {
+        case MessageStateMixin(: final errorMessage?):
+          Messages.showError(errorMessage, context);
+        case MessageStateMixin(: final infoMessage?):
+          Messages.showInfo(infoMessage, context);
+        case MessageStateMixin(: final successMessage?):
+          Messages.showSuccess(successMessage, context);
+      }
+    });
   }
 }
