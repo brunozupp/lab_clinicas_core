f4374dfe300ad675c2d63db48e914fdb39a5a5a6
Bruno Noveli <brunimzuppwolf@gmail.com>
2024-02-14 15:14:57 -0300

    Criado RestClient para web e condicional de import de acordo com a plataforma

--- lib/lab_clinicas_core.dart
+++ lib/lab_clinicas_core.dart
@@ -6,8 +6,6 @@ export 'src/fp/nil.dart';
 
 export 'src/helpers/messages.dart';
 
-export 'src/restClient/rest_client.dart';
-
 export 'src/constants/local_storage_constants.dart';
 
 export 'src/lab_clinicas_core_config.dart';
@@ -20,3 +18,9 @@ export 'src/theme/lab_clinicas_theme.dart';
 
 export 'src/widgets/lab_clinicas_app_bar.dart';
 export 'src/widgets/icon_popup_menu_widget.dart';
+
+// Primeiro ele lê o RestClient normal e depois vai na condicional
+// caso a condicional seja True, ele importa o outro RestClient para web
+// E como ambos tem o mesmo nome, fica transparente para quem vai usar
+export 'src/restClient/rest_client.dart'
+  if(dart.library.html) 'src/restClient/rest_client_web.dart';
